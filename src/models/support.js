65a09970102da3e03c94ad6961dfec8398d8b290
DaEunShim <10ekdms01@naver.com>
2020-11-11 22:01:25 +0900

    feat: [GET] /support 응원 조회 기능 구현

--- src/models/support.js
+++ src/models/support.js
@@ -2,8 +2,9 @@ const pool = require('../modules/pool');
 const table = 'SUPPORT_TB';
 
 exports.postSupport = async(req, support_createdat) => {
-    const query = `INSERT INTO ${table}(support_nickname, support_amount , support_comment, support_phone_number, support_createdat, story_idx) 
-                                VALUES("${req.body.support_nickname}", ${req.body.support_amount}, "${req.body.support_comment}", "${req.body.support_phone_number}", "${support_createdat}", ${req.query.story_idx});`;
+    const query = `INSERT INTO ${table}
+                    (support_nickname, support_amount , support_comment, support_phone_number, support_createdat, story_idx) 
+                    VALUES("${req.body.support_nickname}", ${req.body.support_amount}, "${req.body.support_comment}", "${req.body.support_phone_number}", "${support_createdat}", ${req.query.story_idx});`;
 
     try {
         const result = await pool.queryParam(query);
@@ -15,7 +16,9 @@ exports.postSupport = async(req, support_createdat) => {
 }
 
 exports.getSupportTop = async(req) => {
-    const query = `SELECT support_nickname, support_amount, support_comment FROM ${table} WHERE story_idx = ${req.query.story_idx} LIMIT 3;`;
+    const query = `SELECT support_nickname, support_amount, support_comment 
+                    FROM ${table} WHERE story_idx = ${req.query.story_idx} 
+                    LIMIT 3;`;
 
     try {
         const result = await pool.queryParam(query);
@@ -27,13 +30,46 @@ exports.getSupportTop = async(req) => {
 }
 
 exports.getSupportCount = async(req) => {
-    const query = `SELECT count(*) AS count FROM ${table} WHERE story_idx = ${req.query.story_idx};`;
+    const query = `SELECT count(*) AS count 
+                    FROM ${table} 
+                    WHERE story_idx = ${req.query.story_idx};`;
 
     try {
         const result = await pool.queryParam(query);
         return result[0].count;
     } catch (err) {
         console.log('getSupportCount error: ', err.message);
+        throw err;
+    }
+}
+
+exports.getSupportByRecent = async(req) => {
+    const query = `SELECT support_nickname, support_amount, support_comment 
+                    FROM ${table} 
+                    WHERE story_idx = ${req.query.story_idx} 
+                    ORDER BY support_createdat DESC;`;
+
+    try {
+        const result = await pool.queryParam(query);
+        return result;
+    } catch (err) {
+        console.log('getSupportByRecent error: ', err.message);
+        throw err;
+    }
+}
+
+
+exports.getSupportByAmount = async(req) => {
+    const query = `SELECT support_nickname, support_amount, support_comment 
+                    FROM ${table} 
+                    WHERE story_idx = ${req.query.story_idx} 
+                    ORDER BY support_amount DESC;`;
+
+    try {
+        const result = await pool.queryParam(query);
+        return result;
+    } catch (err) {
+        console.log('getSupportByAmount error: ', err.message);
         throw err;
     }
 }
