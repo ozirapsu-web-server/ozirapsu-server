67c2cee1204d589919aeb0db11726eab120c584e
haerang <gofkdvjvl@naver.com>
2020-11-22 01:22:59 +0900

    feat: cors 설정

--- src/modules/pool.js
+++ src/modules/pool.js
@@ -1,60 +1,60 @@
-const poolPromise = require('../config/database');
+const poolPromise = require("../config/database");
 
 module.exports = {
-    queryParam: async (query) => {
-        return new Promise ( async (resolve, reject) => {
-            try {
-                const pool = await poolPromise;
-                const connection = await pool.getConnection();
-                try {
-                    const result = await connection.query(query);
-                    pool.releaseConnection(connection);
-                    resolve(result);
-                } catch (err) {
-                    pool.releaseConnection(connection);
-                    reject(err);
-                }
-            } catch (err) {
-                reject(err);
-            }
-        });
-    },
-    queryParamArr: async (query, value) => {
-        return new Promise(async (resolve, reject) => {
-            try {
-                const pool = await poolPromise;
-                const connection = await pool.getConnection();
-                try {
-                    const result = await connection.query(query, value);
-                    pool.releaseConnection(connection);
-                    resolve(result);
-                } catch (err) {
-                    pool.releaseConnection(connection);
-                    reject(err);
-                }
-            } catch (err) {
-                reject(err);
-            }
-        });
-    },
-    Transaction: async (...args) => {
-        return new Promise(async (resolve, reject) => {
-            try {
-                const pool = await poolPromise;
-                const connection = await pool.getConnection();
-                try {
-                    await connection.beginTransaction();
-                    const result = args.forEach(async (it) => await it(connection));
-                    await connection.commit();
-                    resolve(result);
-                } catch (err) {
-                    await connection.rollback()
-                    reject(err);
-                }
-                pool.releaseConnection(connection);
-            } catch (err) {
-                reject(err);
-            }
-        });
-    }
-}
+  queryParam: async (query) => {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const pool = await poolPromise;
+        const connection = await pool.getConnection();
+        try {
+          const result = await connection.query(query);
+          pool.releaseConnection(connection);
+          resolve(result);
+        } catch (err) {
+          pool.releaseConnection(connection);
+          reject(err);
+        }
+      } catch (err) {
+        reject(err);
+      }
+    });
+  },
+  queryParamArr: async (query, value) => {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const pool = await poolPromise;
+        const connection = await pool.getConnection();
+        try {
+          const result = await connection.query(query, value);
+          pool.releaseConnection(connection);
+          resolve(result);
+        } catch (err) {
+          pool.releaseConnection(connection);
+          reject(err);
+        }
+      } catch (err) {
+        reject(err);
+      }
+    });
+  },
+  Transaction: async (...args) => {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const pool = await poolPromise;
+        const connection = await pool.getConnection();
+        try {
+          await connection.beginTransaction();
+          const result = args.forEach(async (it) => await it(connection));
+          await connection.commit();
+          resolve(result);
+        } catch (err) {
+          await connection.rollback();
+          reject(err);
+        }
+        pool.releaseConnection(connection);
+      } catch (err) {
+        reject(err);
+      }
+    });
+  },
+};
