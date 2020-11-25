46e7f847bf8eef312e8db80c75190a6d8425be69
Hyeonju Lee <winnerhj1109@naver.com>
2020-11-25 15:34:28 +0900

    feat: 인증 미들웨어 로직 구현

--- src/modules/jwt.js
+++ src/modules/jwt.js
@@ -3,35 +3,65 @@ const { secretKey, options, refreshOptions } = require('../config/secretKey');
 const TOKEN_EXPIRED = -3;
 const TOKEN_INVALID = -2;
 
-module.exports = {
-  sign: async (user) => {
-    const payload = {
-      id: user.id,
-      name: user.userName,
-    };
-    const result = {
-      accessToken: jwt.sign(payload, secretKey, options),
-      refreshToken: jwt.sign(payload, secretKey, refreshOptions),
-    };
-    return result;
-  },
-  verify: async (token) => {
-    let decoded;
-    try {
-      decoded = jwt.verify(token, secretKey);
-    } catch (err) {
-      if (err.message === 'jwt expired') {
-        console.log('expired token');
-        return TOKEN_EXPIRED;
-      } else if (err.message === 'invalid token') {
-        console.log('invalid token');
-        console.log(TOKEN_INVALID);
-        return TOKEN_INVALID;
-      } else {
-        console.log('invalid token');
-        return TOKEN_INVALID;
-      }
+exports.sign = async (user) => {
+  const payload = {
+    idx: user.host_idx,
+  };
+  const result = {
+    accessToken: jwt.sign(payload, secretKey, options),
+    refreshToken: jwt.sign(payload, secretKey, refreshOptions),
+  };
+  return result;
+};
+
+exports.verify = async (token) => {
+  let decoded;
+  try {
+    decoded = jwt.verify(token, secretKey);
+  } catch (err) {
+    if (err.message === 'jwt expired') {
+      console.log('expired token');
+      return TOKEN_EXPIRED;
+    } else if (err.message === 'invalid token') {
+      console.log('invalid token');
+      return TOKEN_INVALID;
+    } else {
+      console.log('invalid token');
+      return TOKEN_INVALID;
     }
-    return decoded;
-  },
+  }
+  return decoded;
+};
+
+exports.refresh = async (refreshToken) => {
+  try {
+    const result = await jwt.verify(refreshToken, secretKey);
+    if (result.id === undefined) {
+      return TOKEN_INVALID;
+    }
+    const user = await userService.getUserId(result.id);
+    if (refreshToken !== user.refreshToken) {
+      console.log('invalid refresh token');
+      return TOKEN_INVALID;
+    }
+    const payload = {
+      idx: user.host_idx,
+    };
+    const dto = {
+      token: jwt.sign(payload, secretKey, options),
+    };
+    return dto;
+  } catch (err) {
+    if (err.message === 'jwt expired') {
+      console.log('expired token');
+      return TOKEN_EXPIRED;
+    } else if (err.message === 'invalid token') {
+      console.log('invalid token');
+      console.log(TOKEN_INVALID);
+      return TOKEN_INVALID;
+    } else {
+      console.log('invalid token');
+      return TOKEN_INVALID;
+    }
+  }
 };
