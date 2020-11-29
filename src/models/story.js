c6b683a0ea7188e3f537756bc132f18e9e7c60c3
Hyeonju Lee <winnerhj1109@naver.com>
2020-11-29 18:48:14 +0900

    fix: 사연 조회에서 호스트 인증 여부를 호스트 한 줄 소개로 변경

--- src/models/story.js
+++ src/models/story.js
@@ -10,7 +10,7 @@ exports.getStoryInfo = async (story_idx) => {
   const query = `SELECT 
                 story_idx as idx, story_title as title, story_content as content, story_link as link,
                 story_target_amount as target_amount, story_current_amount as current_amount, ROUND(story_current_amount*100/story_target_amount, 0) as amount_rate, 
-                story_createdat as created_at, H.host_idx, host_name, host_profile, host_authorized 
+                story_createdat as created_at, H.host_idx, host_name, host_profile, host_introduction 
                 FROM (SELECT * FROM ${story_tb} where story_idx=${story_idx}) as S join HOST_TB as H ON S.host_idx=H.host_idx;`;
 
   try {
